39a8c172307064f77f59f8f51d5d72134c8d74d2
Kalen-ne <54280054+Kalen-ne@users.noreply.github.com>
2020-03-28 18:32:03 +0900

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,6 +1,6 @@
 import discord
 
-TOKEN = 'DISCORD_BOT_TOKEN'
+TOKEN = 'NjYzMjE5MjIxNzQ0MDU4MzY5.Xn8X0A.zSuHZ2T8-NDclqGg3L_XKBxlUwg'
 
 client = discord.Client()
 
@@ -12,4 +12,4 @@ async def on_message(message):
         reply = f'{message.author.mention} ようこそ！'
         await message.channel.send(reply)
 
-client.run(DISCORD_BOT_TOKEN)
+client.run(NjYzMjE5MjIxNzQ0MDU4MzY5.Xn8X0A.zSuHZ2T8-NDclqGg3L_XKBxlUwg)
