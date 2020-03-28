188a094794bb2d78f7882f8cf7aba8fdee4324cd
Kalen-ne <54280054+Kalen-ne@users.noreply.github.com>
2020-03-28 18:19:51 +0900

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,26 @@
+import discord
+
+
+
+TOKEN = 'DISCORD_BOT_TOKEN'
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('ログインしました')
+
+    @client.event
+    async def on_message(message):
+        if message.author.bot:    
+
+@client.event
+async def on_message(message):
+    if message.content.statswith('!kingariempire'):
+        role = discord.utils.get(message.guild.roles, name= '国民')
+        await message.author.add_roles(role)
+        reply = f'{message.author.mention} ようこそ！'
+        await message.channel.send(reply)
+
+client.run(DISCORD_BOT_TOKEN)
---
+++ Procfile
@@ -0,0 +1,1 @@
+app: python Bot.py
